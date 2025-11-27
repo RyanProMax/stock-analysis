63d3ac36e346ce340eb398223b76f72a0770040d
Ryan <627311610@qq.com>
2025-11-28 00:36:35 +0800

    feat: CORS

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 import uvicorn
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from src.controller import index as controller
 
 port = int(os.environ.get("PORT", 8080))
@@ -10,6 +11,20 @@ app = FastAPI(
     title="Stock Analysis API",
     description="An API to perform technical analysis on stock symbols.",
     version="1.0.0",
+)
+
+origins = [
+    "http://localhost:3000",
+    "https://ryanpromax.github.io",
+    "https://ryanai.dev",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  # 允许的域名列表
+    allow_credentials=True,  # 是否允许携带 Cookie 等凭证
+    allow_methods=["*"],  # 允许的方法 (GET, POST, OPTIONS 等)
+    allow_headers=["*"],  # 允许的 Header (Content-Type, Authorization 等)
 )
 
 # Include routers
